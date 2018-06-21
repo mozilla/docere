53270d7677c99887aa7d5013a8eebae2cc984b66
Ryan Harter <harterrt@gmail.com>
2018-06-21 13:31:53 -0400

    Make index prettier using skeleton.css

--- docere/plugins/index/__init__.py
+++ docere/plugins/index/__init__.py
@@ -1,15 +1,22 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 import os
+import shutil
 
 this_dir, this_filename = os.path.split(__file__)
 
 
-with open(os.path.join(this_dir, 'templates/index.html'), 'r') as infile:
-    TEMPLATE = Template(infile.read())
-
+ENV = Environment(loader=FileSystemLoader(
+    os.path.join(this_dir, 'templates/')
+))
+TEMPLATE = ENV.get_template('index.html')
 
 def build_index(reports, directory='.'):
     index = TEMPLATE.render(reports=reports)
 
     with open(os.path.join(directory, 'index.html'), 'w') as outfile:
         outfile.write(index)
+
+    shutil.copytree(
+        os.path.join(this_dir, 'skeleton/css'),
+        os.path.join(directory, 'css')
+    )
