9113d50ca2c2d873fa25b3cb6d49ac5202760210
Ryan Harter <harterrt@gmail.com>
2018-06-21 15:25:44 -0400

    Remove support for python2.7 due to unicode issue

--- docere/plugins/index/__init__.py
+++ docere/plugins/index/__init__.py
@@ -4,11 +4,11 @@ import shutil
 
 this_dir, this_filename = os.path.split(__file__)
 
-
 ENV = Environment(loader=FileSystemLoader(
     os.path.join(this_dir, 'templates/')
 ))
 TEMPLATE = ENV.get_template('index.html')
+
 
 def build_index(reports, directory='.'):
     index = TEMPLATE.render(reports=reports)
