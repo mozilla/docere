3c1b21f11123477f7b612c2c518de44392c1d244
Kimberly Siegler <ksiegler@mozilla.com>
2021-01-13 14:37:57 -0800

    Add data-tag filter for attributes

--- docere/plugins/index/__init__.py
+++ docere/plugins/index/__init__.py
@@ -6,13 +6,13 @@ import shutil
 
 this_dir, this_filename = os.path.split(__file__)
 
-ENV = Environment(loader=FileSystemLoader(
-    os.path.join(this_dir, 'templates/')
-))
-TEMPLATE = ENV.get_template('index.html')
-
-
 def build_index(reports, directory='.'):
+    env = Environment(loader=FileSystemLoader(
+        os.path.join(this_dir, 'templates/')
+    ))
+    env.filters["slugify_string"] = slugify_string
+    template = env.get_template('index.html')
+
     rollup = {
         "products": Counter(),
         "areas": Counter(),
@@ -22,7 +22,7 @@ def build_index(reports, directory='.'):
     for report in reports:
         for k in rollup:
             rollup[k].update(getattr(report, k))
-    index = TEMPLATE.render(reports=reports, slugify=slugify, directory=rollup)
+    index = template.render(reports=reports, directory=rollup, slugify_report=slugify_report)
 
     with open(os.path.join(directory, 'index.html'), 'w') as outfile:
         outfile.write(index)
@@ -38,8 +38,12 @@ def build_index(reports, directory='.'):
     )
 
 
-def slugify(report):
+def slugify_report(report):
     # Returns a string representing the slug for the report.
     report_title = re.sub(r'[^A-Za-z0-9]+', '', report.title)[:32]
     slug = report_title + "_" + report.publish_date.strftime("%Y-%m-%d")
     return slug
+
+
+def slugify_string(s):
+    return re.sub(r'[^A-Za-z0-9]+', '', s)
